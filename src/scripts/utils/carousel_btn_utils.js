4c02a9ff2d51f52626b36fee7b050c026555627e
psy0821-k <p49417875@gmail.com>
2026-02-21 22:09:45 +0900

    feat: api로 데이터 불러오기 테스트 완료 fix: api로 불러왔을 때 button이 작동 안하는 버그 수정

--- src/scripts/utils/carousel_btn_utils.js
+++ src/scripts/utils/carousel_btn_utils.js
@@ -1,57 +1,59 @@
-const buttonContainer = document.querySelector('.button-container');
-const slideImageList = document.querySelector('.movie-item-list');
-const slideImage = slideImageList.querySelectorAll('.movie-item');
-const slideImageWidth = slideImage.item(0).getBoundingClientRect().width;
-const slideImageGap = 24;
-const setIndex = slideImage.length / 3;
-let currentIndex = setIndex;
-const startPoint = (slideImageWidth + slideImageGap) * currentIndex;
+export function btnUtils() {
+  const buttonContainer = document.querySelector('.button-container');
+  const slideImageList = document.querySelector('.movie-item-list');
+  const slideImage = slideImageList.querySelectorAll('.movie-item');
+  const slideImageWidth = slideImage.item(0).getBoundingClientRect().width;
+  const slideImageGap = 24;
+  const setIndex = slideImage.length / 3;
+  let currentIndex = setIndex;
+  const startPoint = (slideImageWidth + slideImageGap) * currentIndex;
 
-function isAnimate(bool) {
-  if (bool) {
-    return (slideImageList.style.transition = `all 0.3s ease-in`);
-  }
-  return (slideImageList.style.transition = `none`);
-}
-
-slideImageList.style.transform = `translateX(-${startPoint}px)`;
-function moveToIndex() {
-  const move = (slideImageWidth + slideImageGap) * -currentIndex;
-  slideImageList.style.transform = `translateX(${move}px)`;
-}
-
-function btn(direction) {
-  if (direction === 'next') {
-    currentIndex += 1;
-
-    if (currentIndex >= setIndex * 2) {
-      setTimeout(() => {
-        isAnimate(false);
-        currentIndex = setIndex;
-        moveToIndex();
-      }, 350);
+  function isAnimate(bool) {
+    if (bool) {
+      return (slideImageList.style.transition = `all 0.3s ease-in`);
     }
+    return (slideImageList.style.transition = `none`);
   }
-  if (direction === 'prev') {
-    currentIndex -= 1;
 
-    if (currentIndex < 1) {
-      setTimeout(() => {
-        isAnimate(false);
-        currentIndex = setIndex;
-        moveToIndex();
-      }, 350);
+  slideImageList.style.transform = `translateX(-${startPoint}px)`;
+  function moveToIndex() {
+    const move = (slideImageWidth + slideImageGap) * -currentIndex;
+    slideImageList.style.transform = `translateX(${move}px)`;
+  }
+
+  function btn(direction) {
+    if (direction === 'next') {
+      currentIndex += 1;
+
+      if (currentIndex >= setIndex * 2) {
+        setTimeout(() => {
+          isAnimate(false);
+          currentIndex = setIndex;
+          moveToIndex();
+        }, 350);
+      }
     }
+    if (direction === 'prev') {
+      currentIndex -= 1;
+
+      if (currentIndex < 1) {
+        setTimeout(() => {
+          isAnimate(false);
+          currentIndex = setIndex;
+          moveToIndex();
+        }, 350);
+      }
+    }
+
+    moveToIndex();
   }
 
-  moveToIndex();
+  buttonContainer.addEventListener('click', (e) => {
+    const button = e.target.closest('button');
+    isAnimate(true);
+
+    if (!button) return;
+    if (button.classList.contains('next-btn')) btn('next');
+    if (button.classList.contains('prev-btn')) btn('prev');
+  });
 }
-
-buttonContainer.addEventListener('click', (e) => {
-  const button = e.target.closest('button');
-  isAnimate(true);
-
-  if (!button) return;
-  if (button.classList.contains('next-btn')) btn('next');
-  if (button.classList.contains('prev-btn')) btn('prev');
-});
