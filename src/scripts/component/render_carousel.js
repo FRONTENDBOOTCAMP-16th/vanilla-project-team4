f4acffb847ac46cd1a523e51a72a848aee7b11af
psy0821-k <p49417875@gmail.com>
2026-02-24 09:53:26 +0900

    fix: 케러셀 방식 변경 및 파일 분할으로 인한 파일 삭제 및 index 파일 수정

--- src/scripts/component/render_carousel.js
+++
@@ -1,8 +0,0 @@
-import { draw } from './recomend_movie_ui';
-export function rederCarousel(type, data, el) {
-  let renderData = data;
-  if (type === 'infinity') {
-    renderData = [...data, ...data, ...data];
-  }
-  draw(renderData, el);
-}
