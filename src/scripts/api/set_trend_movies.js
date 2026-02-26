19bf2d0d1075d0dc3066909d41c27a72a8a6ab24
psy0821-k <p49417875@gmail.com>
2026-02-26 09:10:48 +0900

    feat: 파일 정리 및 버튼 이벤트 위임 방지

--- src/scripts/api/set_trend_movies.js
+++ src/scripts/api/set_trend_movies.js
@@ -3,7 +3,8 @@ import { createMovieList } from '../components/ui/createMovieList';
 import { buttonUtil } from '../utils/carousel/carousel_btn_utils';
 import { addClones } from '../utils/carousel/crousel_clone_node';
 const trandUrl = 'https://api.themoviedb.org/3/trending/all/day?language=ko-KR';
-const inputEl = document.querySelector('.movie-item-list');
+const trendArea = document.querySelector('.trending-movies-area');
+const inputEl = trendArea.querySelector('.movie-item-list');
 getMovieData(trandUrl, 10).then((data) => {
   createMovieList(data, inputEl);
   addClones(inputEl);
