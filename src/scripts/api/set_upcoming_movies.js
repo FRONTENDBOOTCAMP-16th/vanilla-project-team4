733f9e6131970b3d2eada8d45452026be11d6c2b
psy0821-k <p49417875@gmail.com>
2026-02-27 13:46:29 +0900

    feat: 새창에서 열리도록 수정 및 숫자 변수 지정

--- src/scripts/api/set_upcoming_movies.js
+++ src/scripts/api/set_upcoming_movies.js
@@ -1,12 +1,14 @@
+import { countItem } from '../components/count_item';
 import { createMovieList } from '../components/ui/createMovieList';
 import { getTodayMovieData } from '../data/get_today_movie_data';
 
 const upcomingContainer = document.querySelector('.upcoming-area');
 const upcomingList = upcomingContainer.querySelector('.movie-item-list');
 const defaultUrl = 'https://api.themoviedb.org/3/movie/upcoming?language=ko-KR';
-
-getTodayMovieData(defaultUrl, 1, 5)
+const MAX_COUNT = 5;
+getTodayMovieData(defaultUrl, 1, MAX_COUNT)
   .then((data) => {
+    countItem(MAX_COUNT);
     createMovieList(data, upcomingList);
   })
   .catch(console.error);
