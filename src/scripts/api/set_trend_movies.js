a049ac2f764499f840e96ba6092cf2e0be355b8b
psy0821-k <p49417875@gmail.com>
2026-02-27 13:46:29 +0900

    feat: 새창에서 열리도록 수정 및 숫자 변수 지정

--- src/scripts/api/set_trend_movies.js
+++ src/scripts/api/set_trend_movies.js
@@ -2,10 +2,13 @@ import { getMovieData } from '../data/get_movie_data';
 import { createMovieList } from '../components/ui/createMovieList';
 import { buttonUtil } from '../utils/carousel/carousel_btn_utils';
 import { addClones } from '../utils/carousel/crousel_clone_node';
+import { countItem } from '../components/count_item';
 const trandUrl = 'https://api.themoviedb.org/3/trending/all/day?language=ko-KR';
 const trendArea = document.querySelector('.trending-movies-area');
 const inputEl = trendArea.querySelector('.movie-item-list');
-getMovieData(trandUrl, 10).then((data) => {
+const MAX_COUNT = 10;
+getMovieData(trandUrl, MAX_COUNT).then((data) => {
+  countItem(MAX_COUNT);
   createMovieList(data, inputEl);
   addClones(inputEl);
   buttonUtil();
