172988f9692cfc382611ce53e2d7c1a5d6b0b349
psy0821-k <p49417875@gmail.com>
2026-02-27 13:47:40 +0900

    feat: 총 영화갯수 읽어주는 기능과 이미지의 alt와 h3값이 동일함에 따라 수정함

---
+++ src/scripts/components/count_item.js
@@ -0,0 +1,12 @@
+import { createElement } from '../utils/create_element_utils';
+const itemList = document.querySelector('.movie-item-list');
+export function countItem(index) {
+  const p = createElement(
+    'p',
+    ['count-item', 'sr-only'],
+    null,
+    `총 ${index}개의 영화 정보가 있습니다`,
+  );
+
+  itemList.prepend(p);
+}
