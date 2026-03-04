19fbfc0f2430ae37c42d8f55e7817d2e6c0513be
psy0821-k <p49417875@gmail.com>
2026-03-04 11:48:18 +0900

    fix: skipLink 배치 변경

--- src/scripts/components/ui/create_skip_link.js
+++ src/scripts/components/ui/create_skip_link.js
@@ -1,4 +1,4 @@
-import { createElement } from '../../utils/create_element_utils';
+import { createElement } from '/src/scripts/utils/create_element_utils';
 
 export function createSkipLink() {
   const skipLink = createElement(
@@ -10,5 +10,5 @@ export function createSkipLink() {
     '본문으로 바로가기',
   );
 
-  return skipLink;
+  document.body.prepend(skipLink);
 }
